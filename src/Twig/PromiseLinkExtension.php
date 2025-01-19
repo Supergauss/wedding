7866eb02fcb50d619f197c5835eb12bd52b2824b
susele <blondesgift78@gmx.net>
2025-01-19 14:21:24 +0100

    schluss

--- src/Twig/PromiseLinkExtension.php
+++ src/Twig/PromiseLinkExtension.php
@@ -33,15 +33,17 @@ class PromiseLinkExtension extends AbstractExtension
 
         $html = $this->extension->salutation($invitation). ' '.$invitation->getName().",<br><br>";
         if(in_array($invitation->getSalutation()->name, ['MULTI'])){
-            $html .= "Wir laden euch hiermit herzlich zu unserer Hochzeit am 28.05.2025 ein.<br>
-Details findet ihr im Link.<br><br>
-Mit Vorfreude und ganz lieben Grüßen,<br>
+            $html .= "Wir laden euch hiermit herzlich 
+zu unserer Hochzeit am 28.05.2025 
+ein. Details findet ihr im Link.<br>
+Liebe Grüße,<br>
 Susanna & Dennis<br><br>";
         } else {
-            $html .= "Wir laden dich hiermit herzlich zu unserer Hochzeit am 28.05.2025 ein.<br>
-Details findest du im Link.<br><br>
-Mit Vorfreude und ganz lieben Grüßen,<br>
-Susanna & Dennis<br><br>";
+            $html .= "Wir laden dich hiermit herzlich 
+zu unserer Hochzeit am 28.05.2025 
+ein. Details findet ihr im Link.<br>
+Liebe Grüße,<br>
+Susanna & Dennis";
         }
 
          return $html;
@@ -53,7 +55,6 @@ Susanna & Dennis<br><br>";
     {
         $url = $this->router->generate('promise',['id' => $invitation->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
-        //return '<a href="#" onclick="navigator.clipboard.writeText(\''.$url.'\')">Link kopieren für '.$invitation->getName().'</a>';
-        return '<a href="'.$url.'">Link kopieren für '.$invitation->getName().'</a>';
+        return '<a href="#" onclick="navigator.clipboard.writeText(\''.$url.'\')">Link kopieren für '.$invitation->getName().'</a>';
     }
 }
